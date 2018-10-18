4531f6db1a67a7c02eaa0ac0096e184194c6d23a
Nathan Desages <nathan.desages@gmail.com>
2018-10-18 09:06:00 +0200

    ajout sphere buguée

--- Assets/Scripts/Cylinder.cs
+++ Assets/Scripts/Cylinder.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cylinder : MonoBehaviour {
+public class Cylinder : MonoBehaviour
+{
 
 	[Range(3, 100)]
 	public int meridiansNb;
@@ -12,7 +13,8 @@ public class Cylinder : MonoBehaviour {
 
 	int lastMeridiansNb;
 
-	void Start () {
+	void Start ()
+	{
 		lastMeridiansNb = meridiansNb;
 		gameObject.AddComponent<MeshFilter> ();
 		gameObject.AddComponent<MeshRenderer> ();
@@ -20,69 +22,97 @@ public class Cylinder : MonoBehaviour {
 		createCylinderMesh ();
 	}
 
-	void Update() {
+	void Update()
+	{
 		if(meridiansNb != lastMeridiansNb)
 			createCylinderMesh ();
 		lastMeridiansNb = meridiansNb;
 		gameObject.transform.localScale = new Vector3(radius, height, radius);
 	}
 
-	public void createCylinderMesh() {
-		if (meridiansNb >= 3) {
-			Vector3[] vertices = new Vector3[meridiansNb * 2 + 2];
-			for (int m = 0; m < meridiansNb; m++) {
-				float theta = ((2 * Mathf.PI * m) / meridiansNb);
-				vertices [m] = new Vector3 (Mathf.Cos (theta), 0.5f, Mathf.Sin (theta));
-				vertices [m + meridiansNb] = new Vector3 (Mathf.Cos (theta), -0.5f, Mathf.Sin (theta));
-			}
-			vertices [meridiansNb * 2] = new Vector3 (0.0f, 0.5f, 0.0f);
-			vertices [meridiansNb * 2 + 1] = new Vector3 (0.0f, -0.5f, 0.0f);
-
-			int[] triangles = new int[meridiansNb * 12];
-			int t=0;
-			for (int i = 0; i < meridiansNb; i++) {
-				//faces
-				if (i < meridiansNb - 1) {
-					triangles [t++] = i;
-					triangles [t++] = i + meridiansNb;
-					triangles [t++] = i + 1;
-
-					triangles [t++] = i + 1;
-					triangles [t++] = i + meridiansNb;
-					triangles [t++] = i + meridiansNb + 1;
-				} else {
-					triangles [t++] = i;
-					triangles [t++] = i + meridiansNb;
-					triangles [t++] = i - (meridiansNb - 1);
-
-					triangles [t++] = i - (meridiansNb - 1);
-					triangles [t++] = i + meridiansNb;
-					triangles [t++] = i + 1;
-				}
-
-			}
-			for (int i = 0; i < meridiansNb * 2; i++) {
-				//couvercles
-				int A=i;
-				int B, C;
-
-				if (i < meridiansNb) {
-					B = 
-				}
-
-			}
-
-			Mesh msh = new Mesh ();
-
-			msh.vertices = vertices;
-			msh.triangles = triangles;
-			msh.RecalculateNormals ();
-
-			gameObject.transform.localScale = new Vector3(radius, height, radius);
-			gameObject.GetComponent<MeshFilter> ().mesh = msh;
-			gameObject.GetComponent<MeshRenderer> ().material = mat;
-		} else {
-			Debug.Log ("il faut au moins 3 méridiens");
+	public void createCylinderMesh()
+	{
+		Vector3[] vertices = new Vector3[meridiansNb * 2 + 2];
+		for (int m = 0; m < meridiansNb; m++) {
+			float theta = ((2 * Mathf.PI * m) / meridiansNb);
+			vertices [m] = new Vector3 (Mathf.Cos (theta), 0.5f, Mathf.Sin (theta));
+			vertices [m + meridiansNb] = new Vector3 (Mathf.Cos (theta), -0.5f, Mathf.Sin (theta));
 		}
+		vertices [meridiansNb * 2] = new Vector3 (0.0f, 0.5f, 0.0f);
+		vertices [meridiansNb * 2 + 1] = new Vector3 (0.0f, -0.5f, 0.0f);
+
+		int[] triangles = new int[meridiansNb * 12];
+		int t=0;
+		for (int i = 0; i < meridiansNb; i++)
+		{
+			//faces
+			if (i < meridiansNb - 1)
+			{
+				triangles [t++] = i;
+				triangles [t++] = i + 1;
+				triangles [t++] = i + meridiansNb;
+
+				triangles [t++] = i + meridiansNb;
+				triangles [t++] = i + 1;
+				triangles [t++] = i + meridiansNb + 1;
+			}
+			else
+			{
+				triangles [t++] = i;
+				triangles [t++] = i - (meridiansNb - 1);
+				triangles [t++] = i + meridiansNb;
+
+				triangles [t++] = i + meridiansNb;
+				triangles [t++] = i - (meridiansNb - 1);
+				triangles [t++] = i + 1;
+			}
+
+		}
+		for (int i = 0; i < meridiansNb * 2; i++)
+		{
+			//couvercles
+			int A=i;
+			int B, C;
+
+			if ((i+1) % meridiansNb != 0)
+			{
+				if (i < meridiansNb)
+				{
+					B = meridiansNb * 2;
+					C = i + 1;
+				}
+				else
+				{
+					B = i + 1;
+					C = meridiansNb * 2 + 1;
+				}
+			}
+			else
+			{
+				if (i < meridiansNb)
+				{
+					B = meridiansNb * 2;
+					C = i - (meridiansNb - 1);
+				}
+				else
+				{
+					B = i - (meridiansNb - 1);
+					C = meridiansNb * 2 + 1;
+				}
+			}
+			triangles[t++] = A;
+			triangles[t++] = B;
+			triangles[t++] = C;
+		}
+
+		Mesh msh = new Mesh ();
+
+		msh.vertices = vertices;
+		msh.triangles = triangles;
+		msh.RecalculateNormals ();
+
+		gameObject.transform.localScale = new Vector3(radius, height, radius);
+		gameObject.GetComponent<MeshFilter> ().mesh = msh;
+		gameObject.GetComponent<MeshRenderer> ().material = mat;
 	}
 }
--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -23,12 +23,99 @@ public class Sphere : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(meridiansNb != lastMeridiansNb)
-			createSphereMesh ();
+		{
+			Debug.Log("oui");
+			createSphereMesh();
+		}
 		lastMeridiansNb = meridiansNb;
 		gameObject.transform.localScale = new Vector3(radius, radius, radius);
 	}
 
 	public void createSphereMesh () {
-		
+		Vector3[] vertices = new Vector3[(meridiansNb * (meridiansNb - 2)) + 2];
+		int t = 0;
+		for(int i=0; i<meridiansNb-2; i++)
+		{
+			float phi = ((Mathf.PI * (i+1)) / meridiansNb+1);
+			float nr = Mathf.Cos(phi);
+			for (int m = 0; m < meridiansNb; m++)
+			{
+				float theta = ((2 * Mathf.PI * m) / meridiansNb);
+				vertices[t++] = new Vector3(nr * Mathf.Cos(theta), Mathf.Sin(phi), nr * Mathf.Sin(theta));
+			}
+		}
+		vertices[t++] = new Vector3(0, 1, 0);
+		vertices[t++] = new Vector3(0, -1, 0);
+
+		int[] triangles = new int[((meridiansNb-2)*(meridiansNb*2))*3];
+		t = 0;
+		for(int i = 0; i<((meridiansNb*(meridiansNb-3))-1); i++)
+		{
+			if ((i + 1) % meridiansNb != 0)
+			{
+				triangles[t++] = i;
+				triangles[t++] = i + 1;
+				triangles[t++] = i + meridiansNb;
+
+				triangles[t++] = i + meridiansNb;
+				triangles[t++] = i + 1;
+				triangles[t++] = i + meridiansNb + 1;
+			}
+			else
+			{
+				triangles[t++] = i;
+				triangles[t++] = i - (meridiansNb - 1);
+				triangles[t++] = i + meridiansNb;
+
+				triangles[t++] = i + meridiansNb;
+				triangles[t++] = i - (meridiansNb - 1);
+				triangles[t++] = i + 1;
+			}
+		}
+		int A, B, C;
+		for (int i = 0; i < meridiansNb; i++)
+		{
+			A = i;
+			if ((i + 1) % meridiansNb != 0)
+			{
+				B = (meridiansNb * (meridiansNb - 2));
+				C = i + 1;
+			}
+			else
+			{
+				B = (meridiansNb * (meridiansNb - 2));
+				C = i - (meridiansNb - 1);
+			}
+			triangles[t++] = A;
+			triangles[t++] = B;
+			triangles[t++] = C;
+		}
+		for(int i=(meridiansNb*(meridiansNb-3)); i<(meridiansNb*(meridiansNb-2)); i++)
+		{
+			A = i;
+			if ((i + 1) % meridiansNb != 0)
+			{
+				B = i + 1;
+				C = (meridiansNb * (meridiansNb - 2)) + 1;
+			}
+			else
+			{
+				B = i - (meridiansNb - 1);
+				C = (meridiansNb * (meridiansNb - 2)) + 1;
+			}
+			triangles[t++] = A;
+			triangles[t++] = B;
+			triangles[t++] = C;
+		}
+
+		Mesh msh = new Mesh();
+
+		msh.vertices = vertices;
+		msh.triangles = triangles;
+		msh.RecalculateNormals();
+
+		gameObject.transform.localScale = new Vector3(radius, radius, radius);
+		gameObject.GetComponent<MeshFilter>().mesh = msh;
+		gameObject.GetComponent<MeshRenderer>().material = mat;
 	}
 }
